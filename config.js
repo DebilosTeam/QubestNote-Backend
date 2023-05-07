b9d0d57c49c371a8cca308db91428f0e32d42aee
Maksim Bengraf <octobanon@gmail.com>
2023-05-07 23:31:27 +0700

    update: config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-//const MONGODB_URL = "mongodb://root:example@localhost:27017/qubestnote?authSource=admin";
 const SESSION_SECRET_KEY = "9f4a7ec97d66f9a066806724b8b8fd50449e61a8e1ff26c3057c7c2d1874b913";
 
 module.exports = { SESSION_SECRET_KEY };
